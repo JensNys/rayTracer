3922ca4318eba12bfcddee781d1c4e67ad8d5b56
JensNys <jens.nys@student.kuleuven.be>
2024-09-08 15:57:31 +0200

    camera refactoring 1

--- src/Camera.kt
+++ src/Camera.kt
@@ -1,15 +1,28 @@
-class Camera {
-    var origin: Vec3
-    var horizontal: Vec3
-    var vertical: Vec3
-    var lower_left_corner: Vec3
+import java.io.File
+import java.io.FileWriter
 
+class Camera(val aspect_ratio :Double, val image_width :Int, val samplesPerPixel:Int) {
+    //location of the camera in the world
+    var origin: Vec3
+
+    //vector that "points" horizontal bound of the viewport
+    var horizontal: Vec3
+    //vector that "points" vertical bound of the viewport
+    var vertical: Vec3
+    //lower left corner of the viewport
+    var lower_left_corner: Vec3
+    var image_height:Int
 
     init {
-        val aspect_ratio = 16.0 / 9.0
+
+
+        //determine viewport dimensions
+        val focal_length = 1.0
         val viewport_height = 2.0
         val viewport_width = aspect_ratio * viewport_height
-        val focal_length = 1.0
+
+        image_height =  (image_width/aspect_ratio).toInt()
+
 
         origin = Vec3(0, 0, 0)
         horizontal = Vec3(viewport_width, 0.0, 0.0)
@@ -22,7 +35,44 @@ class Camera {
         return Ray(origin, lower_left_corner+ (u*horizontal)+ (v*vertical) - origin)
     }
 
+    fun render(world: Hittable){
+        // render
+        val filename = "sphere1rpp"
+        val myWriter = FileWriter("$filename.ppm")
+        myWriter.write("P3\n")
+        myWriter.write("$image_width $image_height\n255\n")
 
+        for (j in image_height - 1 downTo 0) {
+            print("lines remaining: $j\n")
+            for (i in 0 until image_width) {
+                var pixel_color = Vec3(0, 0, 0)
+                for (s in 0 until samplesPerPixel) {
+                    val u: Double = (i.toDouble() + MyRandom.randomDouble()) / (image_width - 1)
+                    val v: Double = (j.toDouble() + MyRandom.randomDouble()) / (image_height - 1)
+
+                    val r = getRay(u, v)
+
+                    pixel_color = ray_color(r, world).plus(pixel_color)
+                }
+                myWriter.write(pixel_color.writeColor(samplesPerPixel))
+            }
+        }
+        myWriter.close()
+
+        // show ppm picture
+        val currentPath = File(".").canonicalPath
+        val picturePath = "\'$currentPath\\$filename.ppm\'"
+
+        println(System.getProperty("os.name"))
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            val k = Runtime.getRuntime().exec("notepad $filename.ppm")
+        }else{
+            val k = Runtime.getRuntime().exec("gedit $filename.ppm")
+        }
+
+        PPMViewer.display("$filename.ppm")
+
+    }
     /**
      * @returns rgb vector with Vec3 percentage that lerps between white and blue vertically
      */
--- src/RayTracerApplication.kt
+++ src/RayTracerApplication.kt
@@ -4,11 +4,7 @@ import java.io.FileWriter
 fun main() {
 
 
-    //Image
-    val aspect_ratio: Double = 16.0 / 9.0
-    val image_width: Int = 400
-    val image_height: Int = (image_width / aspect_ratio).toInt()
-    val samples_per_pixel: Int = 20
+
 
 
     // World
@@ -19,45 +15,46 @@ fun main() {
     world.add(Plane(Vec3(0.0,1.0,0.0),-0.3))
 
     //Camera
-    val cam: Camera = Camera()
+    val cam: Camera = Camera(16.0 / 9.0,1600,20)
 
+    cam.render(world)
     //file
     // render
-    val filename = "sphere1rpp"
-    val myWriter = FileWriter("$filename.ppm")
-    myWriter.write("P3\n")
-    myWriter.write("$image_width $image_height\n255\n")
-
-    for (j in image_height - 1 downTo 0) {
-        print("lines remaining: $j\n")
-        for (i in 0 until image_width) {
-            var pixel_color = Vec3(0, 0, 0)
-            for (s in 0 until samples_per_pixel) {
-                val u: Double = (i.toDouble() + MyRandom.randomDouble()) / (image_width - 1)
-                val v: Double = (j.toDouble() + MyRandom.randomDouble()) / (image_height - 1)
-
-                val r = cam.getRay(u, v)
-
-                pixel_color = cam.ray_color(r, world).plus(pixel_color)
-            }
-            myWriter.write(pixel_color.writeColor(samples_per_pixel))
-        }
-    }
-    myWriter.close()
-
-
-    // show ppm picture
-    val currentPath = File(".").canonicalPath
-    val picturePath = "\'$currentPath\\$filename.ppm\'"
-
-    println(System.getProperty("os.name"))
-    if (System.getProperty("os.name").startsWith("Windows")) {
-        val k = Runtime.getRuntime().exec("notepad $filename.ppm")
-    }else{
-        val k = Runtime.getRuntime().exec("gedit $filename.ppm")
-    }
-
-    PPMViewer.display("$filename.ppm")
+//    val filename = "sphere1rpp"
+//    val myWriter = FileWriter("$filename.ppm")
+//    myWriter.write("P3\n")
+//    myWriter.write("$image_width $image_height\n255\n")
+//
+//    for (j in image_height - 1 downTo 0) {
+//        print("lines remaining: $j\n")
+//        for (i in 0 until image_width) {
+//            var pixel_color = Vec3(0, 0, 0)
+//            for (s in 0 until samples_per_pixel) {
+//                val u: Double = (i.toDouble() + MyRandom.randomDouble()) / (image_width - 1)
+//                val v: Double = (j.toDouble() + MyRandom.randomDouble()) / (image_height - 1)
+//
+//                val r = cam.getRay(u, v)
+//
+//                pixel_color = cam.ray_color(r, world).plus(pixel_color)
+//            }
+//            myWriter.write(pixel_color.writeColor(samples_per_pixel))
+//        }
+//    }
+//    myWriter.close()
+//
+//
+//    // show ppm picture
+//    val currentPath = File(".").canonicalPath
+//    val picturePath = "\'$currentPath\\$filename.ppm\'"
+//
+//    println(System.getProperty("os.name"))
+//    if (System.getProperty("os.name").startsWith("Windows")) {
+//        val k = Runtime.getRuntime().exec("notepad $filename.ppm")
+//    }else{
+//        val k = Runtime.getRuntime().exec("gedit $filename.ppm")
+//    }
+//
+//    PPMViewer.display("$filename.ppm")
 
 
 }
